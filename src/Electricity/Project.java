03a128c2dc5d4a7ce6acbe647937fd3c6acd76be
Eder Campos Ribeiro <edercribeiro@gmail.com>
2023-02-25 10:32:04 -0300

    Projeto Criado + About, Login and SignUp classes.

---
+++ src/Electricity/Project.java
@@ -0,0 +1,14 @@
+package Electricity;
+
+public class Project {
+
+	public Project(String meter, String user) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setVisible(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
